1abb9136c40abedfe791ab82ace7b6a205723516
Fabio.Oliveira-PC <fabiooli221297@gmail.com>
2022-10-14 12:27:31 -0300

    entering conditionals and return in console

--- CadastroPessoa/Program.cs
+++ CadastroPessoa/Program.cs
@@ -2,10 +2,32 @@
 
 PessoaFisica novaPf = new PessoaFisica();
 
-novaPf.nome = "Fabio";
-novaPf.cpf = "467.847.478-10";
+// novaPf.nome = "Fabio";
+// novaPf.cpf = "467.847.478-10";
 
-Console.WriteLine(novaPf.cpf);
+// Console.WriteLine(novaPf.cpf);
 
-Console.WriteLine("Nome: " + novaPf.nome + " - CPF: " + novaPf.cpf);
-Console.WriteLine($"Nome: {novaPf.nome} - CPF: {novaPf.cpf}");
+// Console.WriteLine("Nome: " + novaPf.nome + " - CPF: " + novaPf.cpf);
+// Console.WriteLine($"Nome: {novaPf.nome} - CPF: {novaPf.cpf}");
+
+float imposotoPagar = novaPf.CalcularImposto(12000);
+Console.WriteLine(imposotoPagar + 1000);
+
+// == igualdade
+// > maior que
+// < menor que
+// >= maior ou igual
+// <= menor ou igual
+// != diferente
+
+// && 'e' - todas as comparacoes sao iguais
+// V && V = V
+// F && V = F
+// V && F = F
+// F && F = F
+
+// || 'ou' - comparacao onde valida uma
+// V || V = V
+// F || V = V
+// V || F = V
+// F || F = V
